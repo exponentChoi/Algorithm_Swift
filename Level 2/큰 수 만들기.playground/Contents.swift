744d7473cee3fd6ccb1dedc5a7982765267309c0
jisu <opkl206@naver.com>
2021-07-02 23:47:01 +0900

    stack으로 풀이, 학습 필요

--- Level 2/큰 수 만들기.playground/Contents.swift
+++ Level 2/큰 수 만들기.playground/Contents.swift
@@ -21,14 +21,14 @@
 
 import Foundation
 
+// 시간초과 발생..
 func solution(_ number:String, _ k:Int) -> String {
-    return greedy(number, letter: "", target: number.count - k)
+    return combine(number, letter: "", target: number.count - k)
         .compactMap { Int($0) }
         .max()!.description
 }
 
-// 중복값 제거 버전
-func greedy(_ number: String, letter: String, target: Int) -> Set<String> {
+func combine(_ number: String, letter: String, target: Int) -> Set<String> {
     if letter.count + number.count < target { return [] }
     if letter.count == target { // (letter - target)이 지우고 난 후의 문자 개수가 동일한 경우 그대로 반환
         return [letter]
@@ -39,32 +39,50 @@ func greedy(_ number: String, letter: String, target: Int) -> Set<String> {
 
     for i in number {
         strings.removeFirst() // 맨 앞자리를 하나씩 지워가면서 재귀함수 호출
-        answer += greedy(strings.joined(), letter: letter + String(i), target: target) // 조합한 문자열 저장
+        answer += combine(strings.joined(), letter: letter + String(i), target: target) // 조합한 문자열 저장
     }
 
     return Set(answer)
 }
 
 
-//func greedy(_ number: String, letter: String, target: Int) -> [String] {
-//    if letter.count + number.count < target { return [] }
-//    if letter.count == target { // (letter - target)이 지우고 난 후의 문자 개수가 동일한 경우 그대로 반환
-//        return [letter]
-//    }
-//
-//    var strings = number.map { String($0) } // String을 배열로 나누기
-//    var answer:[String] = [] // 조합한 문자들을 저장할 곳
-//
-//    for i in number {
-//        strings.removeFirst() // 맨 앞자리를 하나씩 지워가면서 재귀함수 호출
-//        answer += greedy(strings.joined(), letter: letter + String(i), target: target) // 조합한 문자열 저장
-//    }
-//
-//    return answer
-//}
+//print(solution("1924", 2)) // 94
+//print(solution("1231234", 3)) // 3234
+//print(solution("4177252841", 4)) // 775841
+//print(solution("01273456", 4)) // 7456
 
-print(solution("1924", 2)) // 94
-print(solution("1231234", 3)) // 3234
-print(solution("4177252841", 4)) // 775841
 
-print(solution("01273456", 4)) // 7456
+// MARK: - 다른방식으로 접근
+// 첫번째 Index부터 검사하면서 가장 큰 숫자를 저장한다.
+func solution2(_ number:String, _ k:Int) -> String {
+    let nums = number.map { String($0) }
+    var stack = [nums[0]]
+    var count = 0
+    
+    for i in 1..<nums.count {
+        if count == k {
+            stack += nums[i..<nums.count]
+            break
+        }
+        
+        stack.append(nums[i])
+        var last = stack.count - 1
+        
+        if stack[last - 1] < stack[last] {
+            while stack.count != 1 && stack[last - 1] < stack[last] && count < k {
+                stack.swapAt(last, last - 1)
+                stack.removeLast()
+                last = stack.count - 1
+                count += 1
+            }
+        }
+    }
+    
+    return stack.joined()
+}
+
+
+print(solution2("1924", 2)) // 94
+print(solution2("1231234", 3)) // 3234
+print(solution2("4177252841", 4)) // 775841
+print(solution2("01273456", 4)) // 7456
