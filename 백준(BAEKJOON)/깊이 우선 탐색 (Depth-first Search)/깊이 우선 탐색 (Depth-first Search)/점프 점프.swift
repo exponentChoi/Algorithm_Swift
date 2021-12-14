933aadaa1755c3b642c740966a438b201288eeaa
jisu <opkl206@naver.com>
2021-12-14 10:10:53 +0900

    Add solved the 점프 점프 and comment

--- 백준(BAEKJOON)/깊이 우선 탐색 (Depth-first Search)/깊이 우선 탐색 (Depth-first Search)/점프 점프.swift
+++ 백준(BAEKJOON)/깊이 우선 탐색 (Depth-first Search)/깊이 우선 탐색 (Depth-first Search)/점프 점프.swift
@@ -32,42 +32,81 @@
  */
 import Foundation
 
+/**
+ DFS문제로 분류되어 있지만 DFS로 풀이를 해보면 시간초과가 발생한다.
+ */
+
+/// 점프 점프 [14248번]
+//func 점프점프() {
+//    let bridge = Int(readLine()!)! // 돌다리의 돌 개수
+//    let jumps = readLine()!.split(separator: " ").compactMap { Int($0) } // 점프할 수 있는 거리
+//    let start = Int(readLine()!)! - 1 // 출발점
+//    var visits = Set<Int>()
+//
+//    for i in 0..<jumps.count {
+//        var copy = jumps
+//        copy.remove(at: i)
+//        visits = visits.union(dfs(start: start, jump: jumps[i], jumps: copy, bridge: bridge, visits: []))
+//    }
+//
+//    print(visits.count)
+//}
+//
+//func dfs(start: Int, jump: Int, jumps: [Int], bridge: Int, visits: Set<Int>) -> Set<Int> {
+//    if start < 1 || start > bridge { return visits}
+//    if jumps.isEmpty { return Set([start]) }
+//
+//    var visit = visits
+//    visit.insert(start)
+//
+//    for i in 0..<jumps.count {
+//        var copy = jumps
+//        copy.remove(at: i)
+//
+//        if start + jump > bridge || start - jump < 0 || visit.contains(start + jump) || visit.contains(start - jump) {
+//            continue
+//        }
+//
+//        visit = visit.union(dfs(start: start + jump, jump: jumps[i], jumps: copy, bridge: bridge, visits: visit))
+//        visit = visit.union(dfs(start: start - jump, jump: jumps[i], jumps: copy, bridge: bridge, visits: visit))
+//    }
+//
+//    return visit
+//}
+
 /// 점프 점프 [14248번]
 func 점프점프() {
-    let bridge = Int(readLine()!)!
-    let jumps = readLine()!.split(separator: " ").compactMap { Int($0) }
-    let start = Int(readLine()!)! // 출발점
-    var visits = Set<Int>()
+    let bridge = Int(readLine()!)! // 돌다리의 돌 개수
+    let jumps = readLine()!.split(separator: " ").compactMap { Int($0) } // 점프할 수 있는 거리
+    let start = Int(readLine()!)! - 1 // 출발점
+    var visited = (0..<bridge).map { _ in false } // 각 자리마다 방문여부
+    var queue = [Int]() // 돌다리 위에 올라간 후 다음으로 이동하기 위해 대기중인 위치를 저장하는 큐
+    var result = 0
     
-    for i in 0..<jumps.count {
-        var copy = jumps
-        copy.remove(at: i)
-        visits = visits.union(dfs(start: start - 1, jump: jumps[i], jumps: copy, bridge: bridge, visits: []))
-        visits = visits.union(dfs(start: start - 1, jump: -jumps[i], jumps: copy, bridge: bridge, visits: []))
+    func bfs(current: Int) {
+        queue.append(current)
+        visited[current] = true
+        result += 1
+        
+        while !queue.isEmpty { // queue의 대기가 다 사라질 때 까지.
+            let node = queue.removeFirst()
+            
+            for i in [-jumps[node], jumps[node]] { // 왼쪽과 오른쪽으로 가기 위해 음수, 양수를 사용
+                let jump = node + i
+                
+                // 점프한 곳이 (돌다리 범위 밖인 경우 and 아직 방문하지 않은 경우)
+                // 이미 방문한 경우라면 굳이 또 방문할 필요가 없다. (BFS 특징)
+                if 0 <= jump && jump < bridge && visited[jump] == false {
+                    queue.append(jump)
+                    result += 1
+                    visited[jump] = true
+                }
+            }
+        }
     }
     
-    print(visits)
+    bfs(current: start)
+    print(result)
 }
 
-func dfs(start: Int, jump: Int, jumps: [Int], bridge: Int, visits: Set<Int>) -> Set<Int> {
-    if start < 1 || start > bridge { return visits}
-    if jumps.isEmpty { return Set([start]) }
-    
-    var visit = visits
-    visit.insert(start)
-    
-    for i in 0..<jumps.count {
-        var copy = jumps
-        copy.remove(at: i)
-        
-        if start + jump <= bridge {
-            visit = visit.union(dfs(start: start + jump, jump: jumps[i], jumps: copy, bridge: bridge, visits: visit))
-        }
-        
-        if start - jump > 0 {
-            visit = visit.union(dfs(start: start - jump, jump: jumps[i], jumps: copy, bridge: bridge, visits: visit))
-        }
-    }
-    
-    return visit
-}
+
